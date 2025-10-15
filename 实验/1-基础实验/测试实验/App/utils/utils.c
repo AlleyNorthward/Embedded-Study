0f855594b4123a1a1ad80e74fd357a5c50d61b85
AlleyNorthWard <wqianlong93@gmail.com>
2025-10-15 15:15:45 +0800

    日常更新

--- 实验/1-基础实验/测试实验/App/utils/utils.c
+++ 实验/1-基础实验/测试实验/App/utils/utils.c
@@ -1,6 +1,12 @@
 #include "utils.h"
 
 void Heartbeat_LED(void){
+    /*
+        @author 巷北
+        @time 2025.10.14 22:12
+            只能同步进程, 所以外部如果有延时(使用了delay_ms),这里
+        会同步受阻. 后续理解了,更换更好的检测方式.
+    */
     if (!IS_INIT_LED()) LED_Init();
     static u8 i = 0;
     i++;
